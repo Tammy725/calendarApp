41cda8ca404e810429bfc938fc644e72abdb31b7
Tammy Shabetay <tammyshabetay@MacBook-Air-de-Tammy.local>
2026-06-12 17:13:18 -0500

    fix: reducir margen superior a 12

--- app/index.tsx
+++ app/index.tsx
@@ -1594,7 +1594,7 @@ const s1 = StyleSheet.create({
     textTransform: 'uppercase',
     letterSpacing: 0.7,
     marginBottom: 12,
-    marginTop: 16,
+    marginTop: 12,
   },
   heading: {
     fontSize: 26,
